d7c9a1aeba4c1e0a2934691d7834904508b5f528
Sourav Varma Gottumukkala <souravvarma24@gmail.com>
2026-02-06 12:32:13 +0530

    Delete Python : Tasks directory

--- Python : Tasks/5_loops.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    for i in range(n):
-        square = i * i
-        print(square)
--- Python : Tasks/7_print_function.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(input().strip())
-    
-    out = ""
-    for i in range(1, n + 1):
-        out += str(i)
-        
-    print(out)
